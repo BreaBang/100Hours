fa0a9a6b32a7918b67f2d58b0a07e94232d8e4b5
breabang <breanna.bang@gmail.com>
2022-09-10 14:45:21 -0600

    adding pages

--- routes/main.js
+++ routes/main.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const homeController = require('../controllers/home')
+const resourcesController = require('../controllers/myresources')
 
 router.get('/', homeController.getIndex)
+router.get('/', resourcesController.getIndex)
 
 module.exports = router
